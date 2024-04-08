65644f9d789fc28bbaf07b1021a1dfdb7afab092
viperofficailll <sharadhapokharel777@gmail.com>
2024-04-08 12:55:35 +0545

    counter project completed

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -4,26 +4,34 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-const [counter,setcounter]= useState(15)
-const addvalue = ()=>{
-  setcounter(counter+1)
-  
+  const [counter, setcounter] = useState(15)
+  const addvalue = () => {
+    if (counter < 20) { setcounter(counter + 1) }
+    else { setcounter(counter) }
+  }
+
+
+
+
+const subvalue = () => {
+  if (counter > 0) { setcounter(counter - 1) }
+  else {
+
+    counter = counter
+  }
 
 }
-const subvalue =()=>{
-  setcounter(counter-1)
 
- 
-}
-  return (
-    <>
-      <h1> counter </h1>
-      <h1> current value:{counter}</h1>
-      <button onClick={addvalue}>INCREASE VALUE</button>
-      <br></br>
-      <button onClick={subvalue}>DECREASE VALUE</button>
-    </>
-  )
+
+return (
+  <>
+    <h1> counter </h1>
+    <h1> current value:{counter}</h1>
+    <button onClick={addvalue}>INCREASE VALUE</button>
+    <br></br>
+    <button onClick={subvalue}>DECREASE VALUE</button>
+  </>
+)
 }
 
 export default App
