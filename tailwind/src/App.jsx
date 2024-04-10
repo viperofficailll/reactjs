aadb8698106823710553c8ca7b1f382478ab3056
viperofficailll <sharadhapokharel777@gmail.com>
2024-04-10 21:44:34 +0545

    props and tailwind

---
+++ tailwind/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+
+import './App.css'
+import Card from './card'
+
+
+function App() {
+
+
+  return (
+  <>
+  
+  <h1 className='bg-green-500'>
+    chill ma hai
+  </h1> 
+  <br />
+  
+    <Card username="sandesh" btntext="hire me"/><br />
+    
+
+  
+  </>
+
+
+    
+    
+   
+  )
+}
+
+export default App
